1333db1a2a98ae2738f6baeddab5f7c9f0f54a73
sarasagunawardhana <sarasafreelancer@gmail.com>
2022-06-07 22:47:17 +0530

    bad example

--- main.tf
+++ main.tf
@@ -23,3 +23,9 @@ resource "aws_s3_bucket" "dodgyBucket" {
     }
   }
 }
+
+resource "aws_cloudwatch_log_group" "apache_bad" {
+  name              = "apache[${count.index + 1}]"
+  count             = "2"
+  retention_in_days = 15
+}
